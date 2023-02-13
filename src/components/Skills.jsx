4f14357f5c4f54a297ae1e432840bffdc09b1a7f
kcolindres <92882392+kcolindres@users.noreply.github.com>
2023-02-12 22:52:09 -0500

    Update Skills
    Made the squares smaller

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -150,7 +150,7 @@ const Skills = () => {
               key={id}
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
-              <img src={src} alt="" className="w-20 mx-auto" />
+              <img src={src} alt="" className="w-10 mx-auto" />
               <p className="mt-4">{title}</p>
             </div>
           ))}
