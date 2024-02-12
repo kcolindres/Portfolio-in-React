3c3e59830f532edf22edf89c655584c0edc41161
Damo <kcolindres@ufl.edu>
2024-02-12 01:48:25 -0500

    Added more info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,49 @@
 import React from "react";
+import family from "../assets/about/family.png";
+import video from "../assets/about/costarica.mp4";
+import graduation from "../assets/about/graduation.png";
+import soccer from "../assets/about/soccer.png";
+import tennis from "../assets/about/tennis.png";
+import hobbies from "../assets/about/hobbies.png";
 
-const About = () => {
+const Experience = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: video,
+      title: "I was born and raised in Costa Rica",
+    },
+    {
+      id: 2,
+      src: family,
+      title: "Moved to Florida at 14",
+    },
+    {
+      id: 3,
+      src: graduation,
+      title: "I then graduted as first-gen",
+    },
+    {
+      id: 4,
+      src: soccer,
+      title: "Soccer is my favorite sport",
+    },
+    {
+      id: 5,
+      src: tennis,
+      title: "Tennis is my second favorite sport",
+    },
+    {
+      id: 6,
+      src: hobbies,
+      title: "Among other hobbies!",
+    },
+  ];
+
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="bg-gradient-to-b from-gray-800 to-black w-full text-white md:h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
@@ -13,25 +52,37 @@ const About = () => {
           </p>
         </div>
 
-        <p className="text-xl mt-20">
-          My name is Krisna Colindres, a 24-year-old software engineer. I was
-          born and raised in Costa Rica to parents from Honduras and Peru. I'm
-          the eldest brother of two. In 2014, when I was 14 years old, my family
-          immigrated to Florida where I found my second home and amazing
-          friends.
-        </p>
-        <p className="text-xl mt-20">
-          Outside of my studies, I have a range of hobbies and interests. I
-          enjoy playing soccer with my church community, building and learning
-          about mechanical keyboards, and exploring the science of personalities
-          and human interaction. I also love playing video games with friends,
-          hitting the gym, gardening, and keeping up with new technologies.
-        </p>
-
-        <br />
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src, url, title, date }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              {src.endsWith(".mp4") ? (
+                <video
+                  src={src}
+                  autoPlay
+                  loop
+                  muted
+                  className="w-full h-48 object-cover rounded-md duration-200 hover:scale-105"
+                />
+              ) : (
+                <img
+                  src={src}
+                  alt=""
+                  className="w-full h-48 object-cover rounded-md duration-200 hover:scale-105"
+                />
+              )}
+              <div className="flex flex-col items-center justify-center">
+                <button className="w-full px-6 py-3 m-4 duration-200 hover:scale-105 overflow-hidden">
+                  <span className="whitespace-nowrap">
+                    {title} {date}
+                  </span>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-export default About;
+export default Experience;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Software Engineer
+            Hello, I'm a Software Engineer at VISA
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I earned a Bachelor of Science degree in Computer Science from the{" "}
