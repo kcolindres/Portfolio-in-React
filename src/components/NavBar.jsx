ecafdd6cf5cf1b6e0eb7e85b1fdbc93b644ac650
Damo <kcolindres@ufl.edu>
2024-02-12 23:40:25 -0500

    Added more info

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,30 +11,37 @@ const NavBar = () => {
     {
       id: 1,
       link: "home",
+      text: "Home",
     },
     {
       id: 2,
       link: "about",
+      text: "About",
     },
     {
       id: 3,
       link: "skills",
+      text: "Skills",
     },
     {
       id: 4,
       link: "experience",
+      text: "Experience",
     },
     {
       id: 5,
       link: "portfolio",
+      text: "Portfolio",
     },
     {
       id: 6,
       link: "involvement",
+      text: "Involvement",
     },
     {
       id: 7,
       link: "contact",
+      text: "Contact",
     },
   ];
 
@@ -48,23 +55,22 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <a
-          href="#"
-          className="text-5xl font-signature ml-2"
+        <span
+          className="text-5xl font-signature ml-2 cursor-pointer"
           onClick={scrollToTop}
         >
           Krisna Colindres
-        </a>
+        </span>
       </div>
 
       <ul className="hidden md:flex">
-        {links.map(({ id, link }) => (
+        {links.map(({ id, link, text }) => (
           <li
             key={id}
             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
           >
             <Link to={link} smooth duration={500}>
-              {link}
+              {text}
             </Link>
           </li>
         ))}
@@ -79,18 +85,14 @@ const NavBar = () => {
 
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-          {links.map(({ id, link }) => (
+          {links.map(({ id, link, text }) => (
             <li
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
+              onClick={() => setNav(!nav)}
             >
-              <Link
-                onClick={() => setNav(!nav)}
-                to={link}
-                smooth
-                duration={500}
-              >
-                {link}
+              <Link to={link} smooth duration={500}>
+                {text}
               </Link>
             </li>
           ))}
