4f807e2fa8f36afbed5214371e853570d6ccdb4d
Damo <kcolindres@ufl.edu>
2024-02-27 05:01:40 -0500

    Okay everything should be fixed except for spaces between pages and missing demos and some projects

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -134,7 +134,12 @@ const Skills = () => {
   return (
     <div
       name="skills"
-      className="bg-gradient-to-b from-black to-gray-800 w-full min-h-screen" // Use min-h-screen to ensure a minimum height
+      className="bg-gradient-to-b from-black to-gray-800 w-full"
+      style={{
+        minHeight: "100vh",
+        paddingTop: "100px",
+        paddingBottom: "100px",
+      }}
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
@@ -146,7 +151,7 @@ const Skills = () => {
           </p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
